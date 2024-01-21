1ea51150064ffa0d5d45aa172631407fc199494e
unknown <dikshyagautam74@gmail.com>
2024-01-21 21:58:57 +0000

    test2 added

---
+++ Test2.sql
@@ -0,0 +1,7 @@
+Q2. Get the percentage of people who are no longer alive. Alias the result as percentage_dead. Remember to use 100.0 and not 100!
+-- get the count(deathdate) and multiply by 100.0
+-- then divide by count(*)
+
+SELECT COUNT(deathdate)*100.0/count(*) As Percentage_dead
+From people;
+--Test2 added
